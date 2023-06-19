8a25af30b02bddeb18665702a0d7ea7f658f50a4
Marat Sattarov <sattarovmk@yandex.ru>
2023-06-19 11:29:55 +0500

    Chore (page_blocks) unifying IntersectionObs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Welcome from '@/page_blocks/home/Welcome/Welcome';
 import styles from './page.module.scss';
 import Projects from '@/page_blocks/home/Projects/Projects';
-import { RefObject } from 'react';
 
 export default function Home() {
-    observer.current = new IntersectionObserver(callback);
+
     return (
         <main className={styles.main}>
             <Welcome />
