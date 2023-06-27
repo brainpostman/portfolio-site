de2c9481d8357dc7dc51af72a098d5fb70d8d987
Marat Sattarov <sattarovmk@yandex.ru>
2023-06-27 10:46:53 +0500

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Welcome from '@/page_blocks/home/Welcome/Welcome';
+import Welcome from '@/page_blocks/home/welcome/Welcome';
 import styles from './page.module.scss';
-import Projects from '@/page_blocks/home/Projects/Projects';
+import Projects from '@/page_blocks/home/projects/Projects';
 import About from '@/page_blocks/home/About/About';
 
 export default function Home() {
