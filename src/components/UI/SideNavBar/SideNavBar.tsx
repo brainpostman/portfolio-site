f110bf3416a5b4ffbbec9996a17c0c377dd71838
Marat Sattarov <sattarovmk@yandex.ru>
2023-07-09 13:43:32 +0500

    Feat (components) adaptive icon and centered fix

--- src/components/UI/SideNavBar/SideNavBar.tsx
+++ src/components/UI/SideNavBar/SideNavBar.tsx
@@ -2,10 +2,9 @@
 import TextHoverFrame from '@/components/Layout/TextHoverFrame.tsx/TextHoverFrame';
 import { CSSTransition } from 'react-transition-group-react-18';
 import styles from './SideNavBar.module.scss';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import ProjectsIcon from '@p/icon-projects.svg';
 import InfoIcon from '@p/icon-info.svg';
-import { MdOutlinePhone } from 'react-icons/md';
 import UpArrowIcon from '@p/icon-up.svg';
 
 interface SideNavBarProps {
@@ -23,7 +22,6 @@ const transitionStylesNavBar = {
 
 const SideNavBar = ({ inCSSBoolean }: SideNavBarProps) => {
     const navBarRef = useRef<HTMLElement>(null);
-
     const scrollToTop = () => {
         window.scrollTo({
             top: 0,
@@ -37,35 +35,37 @@ const SideNavBar = ({ inCSSBoolean }: SideNavBarProps) => {
     };
 
     return (
-        <CSSTransition
-            in={inCSSBoolean}
-            timeout={200}
-            classNames={transitionStylesNavBar}
-            mountOnEnter
-            unmountOnExit
-            nodeRef={navBarRef}>
-            <nav ref={navBarRef} className={styles.navbar}>
-                <ul className={styles.navbar_list}>
-                    <li onClick={scrollToTop} title={'К началу'}>
-                        <TextHoverFrame>
-                            <UpArrowIcon className={styles.icons} />
-                        </TextHoverFrame>
-                    </li>
-                    <span className={styles.separator} />
-                    <li onClick={() => scrollToBlock('projects')} title={'Проекты'}>
-                        <TextHoverFrame>
-                            <ProjectsIcon className={styles.icons} />
-                        </TextHoverFrame>
-                    </li>
-                    <span className={styles.separator} />
-                    <li onClick={() => scrollToBlock('about')} title={'О себе/Контакты'}>
-                        <TextHoverFrame>
-                            <InfoIcon className={styles.icons} />{' '}
-                        </TextHoverFrame>
-                    </li>
-                </ul>
-            </nav>
-        </CSSTransition>
+        <div className={styles.container}>
+            <CSSTransition
+                in={inCSSBoolean}
+                timeout={200}
+                classNames={transitionStylesNavBar}
+                mountOnEnter
+                unmountOnExit
+                nodeRef={navBarRef}>
+                <nav ref={navBarRef} className={styles.navbar}>
+                    <ul className={styles.navbar_list}>
+                        <li onClick={scrollToTop} title={'К началу'}>
+                            <TextHoverFrame>
+                                <UpArrowIcon className={styles.icons} />
+                            </TextHoverFrame>
+                        </li>
+                        <span className={styles.separator} />
+                        <li onClick={() => scrollToBlock('projects')} title={'Проекты'}>
+                            <TextHoverFrame>
+                                <ProjectsIcon className={styles.icons} />
+                            </TextHoverFrame>
+                        </li>
+                        <span className={styles.separator} />
+                        <li onClick={() => scrollToBlock('about')} title={'О себе/Контакты'}>
+                            <TextHoverFrame>
+                                <InfoIcon className={styles.icons} />{' '}
+                            </TextHoverFrame>
+                        </li>
+                    </ul>
+                </nav>
+            </CSSTransition>
+        </div>
     );
 };
 
